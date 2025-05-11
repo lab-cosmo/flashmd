0aae6970f3a89f114429aaeac8792c514d315761
Michele Ceriotti <michele.ceriotti@gmail.com>
2025-05-11 16:23:14 +0200

    Cleaned up the implementation moving out the barostat helpers

--- src/skipmd/ipi.py
+++ src/skipmd/ipi.py
@@ -80,6 +80,7 @@ def get_nve_stepper(sim, model, device, rescale_energy=True, random_rotation=Fal
 
     return nve_stepper
 
+    
 def get_nvt_stepper(sim, model, device, rescale_energy=True, random_rotation=False):
     motion = sim.simulation.syslist[0].motion
     if type(motion.integrator) is not NVTIntegrator:
@@ -97,6 +98,38 @@ def get_nvt_stepper(sim, model, device, rescale_energy=True, random_rotation=Fal
 
     return nvt_stepper
 
+def _qbaro(baro):
+    """Propagation step for the cell volume (adjusting atomic positions and momenta)."""
+
+    v = baro.p[0] / baro.m[0]
+    halfdt = (
+        baro.qdt
+    )  # this is set to half the inner loop in all integrators that use a barostat
+    expq, expp = (np.exp(v * halfdt), np.exp(-v * halfdt))
+
+    m = dstrip(baro.beads.m3)[0]
+
+    baro.nm.qnm[0, :] *= expq
+    baro.nm.pnm[0, :] *= expp
+    baro.cell.h *= expq
+
+def _pbaro(baro):
+    """Propagation step for the cell momentum (adjusting atomic positions and momenta)."""
+
+    # we are assuming then that p the coupling between p^2 and dp/dt only involves the fast force
+    dt = baro.pdt[0]
+
+    # computes the pressure associated with the forces at the outer level MTS level.
+    press = np.trace(baro.stress_mts(0)) / 3.0
+    # integerates the kinetic part of the pressure with the force at the inner-most level.
+    nbeads = baro.beads.nbeads
+    baro.p += (
+        3.0 * dt * (
+            baro.cell.V * (press - nbeads * baro.pext) + Constants.kb * baro.temp
+        )
+    )
+
+
 def get_npt_stepper(sim, model, device, rescale_energy=True, random_rotation=False):
     motion = sim.simulation.syslist[0].motion
     if type(motion.integrator) is not NPTIntegrator:
@@ -107,48 +140,17 @@ def get_npt_stepper(sim, model, device, rescale_energy=True, random_rotation=Fal
 
     # The barostat here needs a simpler splitting than for BZP, something as 
     # OAbBbBABbAbPO where Bp and Ap are the cell momentum and volume steps
-    def qbaro(baro):
-        """Propagation step for the cell (adjusting atomic positions and momenta)."""
-
-        v = baro.p[0] / baro.m[0]
-        halfdt = (
-            baro.qdt
-        )  # this is set to half the inner loop in all integrators that use a barostat
-        expq, expp = (np.exp(v * halfdt), np.exp(-v * halfdt))
-
-        m = dstrip(baro.beads.m3)[0]
-
-        baro.nm.qnm[0, :] *= expq
-        baro.nm.pnm[0, :] *= expp
-        baro.cell.h *= expq
-
-    def pbaro(baro):
-        # we are assuming then that p the coupling between p^2 and dp/dt only involves the fast force
-        dt = baro.pdt[0]
-
-        # computes the pressure associated with the forces at the outer level MTS level.
-        press = np.trace(baro.stress_mts(0)) / 3.0
-        # integerates the kinetic part of the pressure with the force at the inner-most level.
-        nbeads = baro.beads.nbeads
-        baro.p += (
-            3.0 * dt * (
-                baro.cell.V * (press - nbeads * baro.pext) + Constants.kb * baro.temp
-            )
-        )
-
-    
     def npt_stepper(motion, *_, **__):
-        # OOpApBpBABBpApOpO splitting of a NpT propagator        
         motion.thermostat.step()
         motion.integrator.pconstraints()
         motion.barostat.thermostat.step()
-        qbaro(motion.barostat)
-        pbaro(motion.barostat)
+        _qbaro(motion.barostat)
+        _pbaro(motion.barostat)
 
         flashmd_vv_step(motion)
 
-        pbaro(motion.barostat)
-        qbaro(motion.barostat)
+        _pbaro(motion.barostat)
+        _qbaro(motion.barostat)
         motion.barostat.thermostat.step()
         motion.thermostat.step()
         motion.integrator.pconstraints()
