d18bc927b856abd5bb4ac3f7ae6e26797702bbf0
Michele Ceriotti <michele.ceriotti@gmail.com>
2025-05-25 12:11:03 +0200

    Raise an error if the time step is not available
    Better than a cryptic missing file error

--- src/flashmd/models.py
+++ src/flashmd/models.py
@@ -3,7 +3,11 @@ from metatensor.torch.atomistic import MetatensorAtomisticModel, load_atomistic_
 
 
 def get_universal_model(time_step: int = 16) -> MetatensorAtomisticModel:
-    
+
+    if (time_step not in [1, 4, 16, 64]):
+        raise ValueError("Univeral FlashMD models are only available for"
+                         " time steps of 1fs, 4fs, 16fs, 64fs.")
+
     model_path = hf_hub_download(
         repo_id="lab-cosmo/flashmd",
         filename=f"flashmd_{time_step}fs.pt",
