0089f335046e20f24dea322797eff20632a1334e
Michele Ceriotti <ceriottm@users.noreply.github.com>
2025-05-25 12:12:09 +0200

    Added utilities to help create energy-rescaled VV integrators (#5)

--- src/flashmd/__init__.py
+++ src/flashmd/__init__.py
@@ -1,2 +1,3 @@
 import warnings
+
 warnings.filterwarnings("ignore", category=UserWarning, message="custom data")
--- src/flashmd/utils/energy_error.py
+++ src/flashmd/utils/energy_error.py
@@ -1,5 +1,9 @@
 import ase.io
-from metatensor.torch.atomistic import load_atomistic_model, systems_to_torch, ModelEvaluationOptions
+from metatensor.torch.atomistic import (
+    load_atomistic_model,
+    systems_to_torch,
+    ModelEvaluationOptions,
+)
 from metatensor.torch.atomistic.ase_calculator import MetatensorCalculator
 import torch
 from ase.md import VelocityVerlet
@@ -16,10 +20,14 @@ from metatensor.torch import Labels, TensorBlock, TensorMap
 
 def get_energy_error(file_path, model_path, flashmd_model_path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    calculator = MetatensorCalculator(model_path, extensions_directory="extensions_pet_mad/", device=device)
+    calculator = MetatensorCalculator(
+        model_path, extensions_directory="extensions_pet_mad/", device=device
+    )
     flashmd_model = load_atomistic_model(flashmd_model_path)
 
-    delta_q_key = [k for k in flashmd_model.capabilities().outputs.keys() if "mtt::delta_" in k][0]
+    delta_q_key = [
+        k for k in flashmd_model.capabilities().outputs.keys() if "mtt::delta_" in k
+    ][0]
     n_steps = int(delta_q_key.split("_")[1].split("_")[0])
 
     stepper = FlashMDStepper([flashmd_model], n_steps, torch.device(device))
@@ -32,23 +40,28 @@ def get_energy_error(file_path, model_path, flashmd_model_path):
     for structure in tqdm.tqdm(structures):
         atoms = copy.deepcopy(structure)
         atoms.calc = calculator
-        energies_md.append(atoms.get_total_energy()/len(atoms))
+        energies_md.append(atoms.get_total_energy() / len(atoms))
         system = _atoms_to_system(atoms, device)
 
         new_system = stepper.step(system)
 
         atoms_flashmd = _system_to_atoms(new_system)
         atoms_flashmd.calc = calculator
-        energies_flashmd.append(atoms_flashmd.get_total_energy()/len(atoms))
+        energies_flashmd.append(atoms_flashmd.get_total_energy() / len(atoms))
 
-    rmse = np.sqrt(np.mean((np.array(energies_md) - np.array(energies_flashmd))**2))
+    rmse = np.sqrt(np.mean((np.array(energies_md) - np.array(energies_flashmd)) ** 2))
 
     # plot scatter plot
     plt.scatter(energies_md, energies_flashmd, s=1)
     plt.xlabel("MD energy")
     plt.ylabel("FlashMD energy")
     plt.title(f"FlashMD vs MD energy (RMSE: {rmse:.8f})")
-    plt.plot([min(energies_md), max(energies_md)], [min(energies_md), max(energies_md)], color='red', linestyle='--')
+    plt.plot(
+        [min(energies_md), max(energies_md)],
+        [min(energies_md), max(energies_md)],
+        color="red",
+        linestyle="--",
+    )
     plt.tight_layout()
     plt.savefig("energy_error.pdf")
 
@@ -61,37 +74,51 @@ def _atoms_to_system(atoms, device):
         "momenta",
         TensorMap(
             keys=Labels.single().to(device),
-            blocks = [
+            blocks=[
                 TensorBlock(
-                    values=torch.tensor(atoms.get_momenta(), dtype=torch.float32, device=device).unsqueeze(-1),
+                    values=torch.tensor(
+                        atoms.get_momenta(), dtype=torch.float32, device=device
+                    ).unsqueeze(-1),
                     samples=Labels(
                         names=["system", "atom"],
-                        values=torch.tensor([[0, j] for j in range(len(atoms))], device=device),
+                        values=torch.tensor(
+                            [[0, j] for j in range(len(atoms))], device=device
+                        ),
                     ),
-                    components=[Labels(names="xyz", values=torch.tensor([[0], [1], [2]], device=device))],
+                    components=[
+                        Labels(
+                            names="xyz",
+                            values=torch.tensor([[0], [1], [2]], device=device),
+                        )
+                    ],
                     properties=Labels.single().to(device),
                 )
             ],
-        )
+        ),
     )
     system.add_data(
         "masses",
         TensorMap(
             keys=Labels.single().to(device),
-            blocks = [
+            blocks=[
                 TensorBlock(
-                    values=torch.tensor(atoms.get_masses(), dtype=torch.float32, device=device).unsqueeze(-1),
+                    values=torch.tensor(
+                        atoms.get_masses(), dtype=torch.float32, device=device
+                    ).unsqueeze(-1),
                     samples=Labels(
                         names=["system", "atom"],
-                        values=torch.tensor([[0, j] for j in range(len(atoms))], device=device),
+                        values=torch.tensor(
+                            [[0, j] for j in range(len(atoms))], device=device
+                        ),
                     ),
                     components=[],
                     properties=Labels.single().to(device),
                 )
             ],
-        )
+        ),
     )
     return system
+
 
 def _system_to_atoms(system):
     atomic_numbers = system.types.cpu().numpy()
