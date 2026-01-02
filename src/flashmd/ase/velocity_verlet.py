fa0981a3744f8983a788c14d02e1b642a2eb90d7
frostedoyster <bigi.f@libero.it>
2026-01-02 22:20:26 +0100

    Fix for ASE 3.27.0

--- src/flashmd/ase/velocity_verlet.py
+++ src/flashmd/ase/velocity_verlet.py
@@ -97,54 +97,35 @@ class VelocityVerlet(MolecularDynamics):
 
     def irun(self, steps=50):
         # We have to override irun to avoid calling MolecularDynamics.irun(), which
-        # itself calls Dynamics.irun(), which assumes an optimizer-like behavior and
-        # call gradients to check convergence. This function is a copy of
-        # Dynamics.irun(), where gradients are never requested and set to None.
+        # calls gradients to check convergence (optimizer-like behavior) or to log the
+        # forces, depending on the ASE version. This function is a copy of
+        # Dynamics.irun(), where the calls to the forces are commented out.
 
         # update the maximum number of steps
         self.max_steps = self.nsteps + steps
 
-        # compute the initial step
-        gradient = None  # used to be gradient = self.optimizable.get_gradient()
-
-        # log the initial step
         if self.nsteps == 0:
-            self.log(gradient)
-
-            # we write a trajectory file if it is None
-            if self.trajectory is None:
-                self.call_observers()
-            # We do not write on restart w/ an existing trajectory file
-            # present. This duplicates the same entry twice
-            elif len(self.trajectory) == 0:
-                self.call_observers()
-
-        # check convergence
-        gradient = None  # used to be gradient = self.optimizable.get_gradient()
-        is_converged = self.converged(gradient)
-        yield is_converged
-
-        # run the algorithm until converged or max_steps reached
-        while not is_converged and self.nsteps < self.max_steps:
-            # compute the next step
-            self.step()
-            self.nsteps += 1
-
-            # log the step
-            gradient = None  # used to be gradient = self.optimizable.get_gradient()
-            self.log(gradient)
+            # For historical reasons we do a magical incantation
+            # here with forces, log, observers.
+            # self.atoms.get_forces()
+            self.log()
             self.call_observers()
 
-            # check convergence
-            gradient = None  # used to be gradient = self.optimizable.get_gradient()
-            is_converged = self.converged(gradient)
-            yield is_converged
+        yield self.nsteps == self.max_steps
+
+        # run the algorithm until converged or max_steps reached
+        while self.nsteps < self.max_steps:
+            self.step()
+            self.nsteps += 1
+            # self.atoms.get_forces()
+            self.log()
+            self.call_observers()
+            yield self.nsteps == self.max_steps
 
     def run(self, steps=50):
-        # Same motivation as irun(): override to avoid optimizer-like calls to forces.
-        for converged in self.irun(steps=steps):
+        # needed for ASE <= 3.26.0; in 3.27.0 Dynamics.run() works well for us
+        for _ in self.irun(steps=steps):
             pass
-        return converged
 
 def _convert_atoms_to_system(
     atoms: ase.Atoms, dtype: str, device: str | torch.device
