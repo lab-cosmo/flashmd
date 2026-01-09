6c0da1d607e09020416485e64785a4bf79f753b8
frostedoyster <bigi.f@libero.it>
2026-01-09 19:05:35 +0100

    Add helper to get checkpoints for fine-tuning

--- src/flashmd/__init__.py
+++ src/flashmd/__init__.py
@@ -1,6 +1,7 @@
 import warnings
 
 from .models import get_pretrained as get_pretrained
+from .models import save_checkpoint as save_checkpoint
 
 
 warnings.filterwarnings("ignore", category=UserWarning, message="custom data")
