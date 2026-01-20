872b060fd99371f274f9722e1fa3284e342f9659
Johannes Spies <13813209+johannes-spies@users.noreply.github.com>
2026-01-20 15:59:35 +0000

    Add another solver

--- examples/al/simulation-flashmd-symplectic/run.py
+++ examples/al/simulation-flashmd-symplectic/run.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import torch
 from metatomic.torch import load_atomistic_model
 from ipi.utils.scripting import InteractiveSimulation
@@ -5,6 +6,7 @@ from flashmd.steppers import SymplecticStepper
 from flashmd.stepper import FlashMDStepper
 from flashmd.vv import flashmd_vv
 from flashmd.wrappers import wrap_nvt
+from flashmd.fpi import anderson_solver
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -20,8 +22,20 @@ initial_guess = FlashMDStepper(flashmd_model_32, device=device)
 flashmd_symplectic_model_32 = load_atomistic_model("../models/flashmd-symplectic.pt")
 flashmd_symplectic_model_32.to(device)
 
+# create a fixed-point solver and attach a logger to see the convergence behavior
+solver_kwargs = dict(m=0, max_iter=100, tol=1e-3, beta=0.5)
+def solver_with_log(
+    g: Callable[[torch.Tensor], torch.Tensor],
+    x0: torch.Tensor,
+) -> torch.Tensor:
+    x_star, norms = anderson_solver(g, x0, return_residual_norms=True, **solver_kwargs)  # type: ignore
+    print("l2 accuracies (converged in %d steps):" % len(norms))
+    for i, n in enumerate(norms):
+       print("iteration", i, "residual norm:", n)
+    return x_star
+
 # replace the motion step with a FlashMD stepper
-stepper = SymplecticStepper(initial_guess, flashmd_symplectic_model_32, None)
+stepper = SymplecticStepper(initial_guess, flashmd_symplectic_model_32, solver_with_log)
 step_fn = flashmd_vv(sim, stepper, device=device, dtype=torch.float32, rescale_energy=False, random_rotation=False)
 step_fn = wrap_nvt(sim, step_fn)
 sim.set_motion_step(step_fn)
