3f1417a836f6fb7eba90d257d1e9f5d2aabd03d9
frostedoyster <bigi.f@libero.it>
2026-02-16 10:27:43 +0100

    Try again

--- src/flashmd/__init__.py
+++ src/flashmd/__init__.py
@@ -5,5 +5,5 @@ from .models import save_checkpoint as save_checkpoint
 
 
 warnings.filterwarnings("ignore", category=UserWarning, message="custom data")
-warnings.filterwarnings("ignore", category=DeprecationWarning, message="jit.script")
-warnings.filterwarnings("ignore", category=DeprecationWarning, message="jit.load")
+warnings.filterwarnings("ignore", category=DeprecationWarning, message="`torch.jit.script`")
+warnings.filterwarnings("ignore", category=DeprecationWarning, message="`torch.jit.load`")
