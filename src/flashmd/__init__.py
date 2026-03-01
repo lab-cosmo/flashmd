f2eb0ca9cb9f98995abe83c9ff23966707a08062
frostedoyster <bigi.f@libero.it>
2026-03-01 11:44:08 +0100

    Fix location of jit settings

--- src/flashmd/__init__.py
+++ src/flashmd/__init__.py
@@ -1,5 +1,7 @@
 import warnings
 
+import torch
+
 from .models import get_pretrained as get_pretrained
 from .models import save_checkpoint as save_checkpoint
 
@@ -11,3 +13,9 @@ warnings.filterwarnings(
 warnings.filterwarnings(
     "ignore", category=DeprecationWarning, message="`torch.jit.load`"
 )
+
+
+# Disable static fusion. Besides the fact that atomistic batches have variable
+# sizes, statically fused CUDA kernels cannot allocate new tensors at runtime,
+# causing "Global alloc not supported yet" errors (cuda 13+) at the time of writing
+torch.jit.set_fusion_strategy([("DYNAMIC", 10)])
